99f091a67d5ae0d1235d812fb96fe3b533618e36
Shadrack K Kimutai <shadrackkimutai@gmail.com>
2024-01-08 19:08:56 +0300

    Working example with scroll able page

--- testselenium.py
+++ testselenium.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 browser = webdriver.Chrome()
 browser.get("https://www.google.com")
 s=browser.current_url
-print(s.replace('https://',''))
+s=s.replace('https://','')
 print(s.replace('/',''))
 
 #browser.save_screenshot("link"+browser.current_url+".png")
