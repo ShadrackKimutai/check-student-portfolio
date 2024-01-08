38823c375c52c91c1a81f7bbd9d9b6f898980b42
Shadrack K Kimutai <shadrackkimutai@gmail.com>
2024-01-08 15:42:58 +0300

    strip forward slash

--- testselenium.py
+++ testselenium.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Chrome()
 browser.get("https://www.google.com")
-print(str.replace('https://',browser.current_url))
+s=browser.current_url
+print(s.replace('https://',''))
+print(s.replace('/',''))
+
 #browser.save_screenshot("link"+browser.current_url+".png")
 browser.close()
